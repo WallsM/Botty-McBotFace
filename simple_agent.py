2addb94e2b3049c2ffa9c4394d14ed576ab094fa
potato_farmer <wildtangles@gmail.com>
2017-09-13 18:33:55 +0100

    Using SC2Env now

--- simple_agent.py
+++ simple_agent.py
@@ -1,18 +1,32 @@
-"""
-Docstring dfsdfs dfsd.
+#TODO1: Off by 1 error for step count?
 
-fsdfsdfsd fds
-fsd
-"""
-from pysc2.agents import base_agent
+import sys
+import gflags as flags
+
 from pysc2.lib import actions
-from time import sleep
+from pysc2.env import sc2_env
+from pysc2.env import environment
 
+FLAGS = flags.FLAGS
 
-class SimpleAgent(base_agent.BaseAgent):
-    ''' stop being annoying docstring '''
-    def step(self, obs):
-        super(SimpleAgent, self).step(obs)
+class Agent:
 
-        sleep(0.5)
+    def __init__(self):
+        FLAGS(sys.argv)
+        self.env =  sc2_env.SC2Env('Simple64', agent_race = "T", visualize=True)
+
+    def think(self, obs):
         return actions.FunctionCall(actions.FUNCTIONS.no_op.id, [])
+
+    def play(self):
+        obs = self.env.reset()
+
+        while obs[0].step_type != environment.StepType.LAST:
+            obs = self.env.step(actions = [self.think(obs)])
+        # TODO1: DO WE ALWAYS MISS THE LAST STEP HERE?
+
+        self.env.close()
+
+if __name__ == "__main__":
+    ben = Agent()
+    ben.play()
